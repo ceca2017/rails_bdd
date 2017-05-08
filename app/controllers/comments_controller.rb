d7a4af8cacb4e879c5f7f91b957a5b57037b5289
claes <claesca@outlook.com>
2017-05-08 20:04:54 +0200

    able to see added comment for article

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,8 +2,16 @@ class CommentsController < ApplicationController
 
   def create
     @article = Article.find(params[:article_id])
-    @comment = @article.comments.new
+    # @comment = @article.comments.new
+    @article.comments.create(comment_params)
     redirect_to article_path(@article)
+    binding.pry
+  end
+
+  private
+
+  def comment_params
+    params[:comment].permit(:comment, :email)
   end
 
 end
